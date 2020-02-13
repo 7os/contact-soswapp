ac5bf8506cae11b6f92fa4bf69f49545e75ab7f4
Sir Louis <me@sirlouis.io>
2020-02-13 04:41:04 +0100

    bug fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $page_name = "contactus";
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
-    <?php \setup_page('contact-us','base', true); ?>
+    <?php \setup_page('contact-us','base', true, PRJ_HEADER_HEIGHT); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
       <div class="view-space">
